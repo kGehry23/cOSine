c8ed9b8bfd031a3658ab998fc366680e88b08811
Kai Gehry <kaigehry@gmail.com>
2025-12-18 18:44:13 -0800

    bash script to boot os created

---
+++ boot/boot_qemu.sh
@@ -0,0 +1,14 @@
+# /**
+#  ********************************************************************************
+#  * @file    boot_qemu.sh
+#  * @author  Kai Gehry
+#  * @date    2025-12-18
+#  *
+#  * @brief   Boots qemu with os image.
+#  ********************************************************************************
+#  */
+
+#Change to qemu directory
+cd "c:/Program Files/qemu"
+#Execute CD-ROM image for OS
+eval "./qemu-system-x86_64.exe"
