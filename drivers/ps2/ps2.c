9bea2d29213e5b98b1c917d11e561c17c59bb0e8
Kai Gehry <kaigehry@gmail.com>
2026-01-10 19:54:23 -0800

    PS2 controller driver almost complete. Need configure PIC before continuing

--- drivers/ps2/ps2.c
+++ drivers/ps2/ps2.c
@@ -2,7 +2,25 @@
 #include "../../libc/stdio/stdio.h"
 
 /*!
- * @brief Initializes the PS/2 Controller
+ * @brief Checks bit 0 of the PS/2 status register until set to 1
+ * @return None
+ */
+void poll_status_bit_0(void)
+{
+    while(inb(PS2_READ_STATUS_PORT)&0x1 != 1);
+}
+
+/*!
+ * @brief Checks bit 1 of the PS/2 status register until set to 0
+ * @return None
+ */
+void poll_status_bit_1(void)
+{
+    while(((inb(PS2_READ_STATUS_PORT) >> 1)&0x1) != 0);
+}
+
+/*!
+ * @brief Initializes and tests the PS/2 Controller
  * @return None
  */
 void init_ps2_controller()
@@ -11,6 +29,10 @@ void init_ps2_controller()
     set_config_byte();
     test_ps2_controller();
     test_dual_channel();
+    test_ports();
+    enable_devices();
+
+    reset_devices();
 }
 
 /*!
@@ -36,7 +58,7 @@ void set_config_byte()
     uint8_t config_byte = inb(PS2_READ_WRITE_DATA_PORT);
 
     //Modifies the config byte. Clears bits 0, 4, and 6
-    config_byte = config_byte & 0xAE;
+    config_byte &= 0xAE;
 
     //Writes the updated config byte to the data port
     outb(PS2_WRITE_COMMAND_PORT, WRITE_CONTROLLER_CONFIG_BYTE);
@@ -52,13 +74,12 @@ void test_ps2_controller()
     //Writes the test byte to the controller command port
     outb(PS2_WRITE_COMMAND_PORT, TEST_CONTROLLER_BYTE);
 
-    //Continue to check the port while bit 0 in the status register is not set
-    while(inb(PS2_READ_STATUS_PORT)&0x1 != 1);
+    poll_status_bit_0();
 
     uint8_t test_byte = inb(PS2_READ_WRITE_DATA_PORT);
     printf("PS/2 self test: ");
 
-    if(test_byte == 0x55)
+    if(test_byte == SELF_TEST_PASSED)
         printf("%p -> Test Passed.\n", test_byte);
     else
         printf("%p -> Test Failed.\n", test_byte);    
@@ -70,13 +91,11 @@ void test_ps2_controller()
  */
 void test_dual_channel()
 {
-    //
     outb(PS2_WRITE_COMMAND_PORT, ENABLE_PS2_PORT_2);
     //Reads the config byte
     outb(PS2_WRITE_COMMAND_PORT, READ_CONTROLLER_CONFIG_BYTE);
 
-    //Continue to check the port while bit 0 in the status register is not set
-    while(inb(PS2_READ_STATUS_PORT)&0x1 != 1);
+    poll_status_bit_0();
 
     uint8_t config_byte = inb(PS2_READ_WRITE_DATA_PORT);
 
@@ -91,11 +110,114 @@ void test_dual_channel()
     //Disable second channel ps2 port after test
     outb(PS2_WRITE_COMMAND_PORT, DISABLE_PS2_PORT_2);  
 
-    //Modifies the config byte. Disables IRQs and enables the clock for port 2
-    config_byte = config_byte & 0xDD;
+    //Modifies the config byte. Clears bits 1 and 5, disabling IRQs and enabling the clock for port 2
+    config_byte &= 0xDD;
     //Writes the updated config byte to the data port
     outb(PS2_WRITE_COMMAND_PORT, WRITE_CONTROLLER_CONFIG_BYTE);
     outb(PS2_READ_WRITE_DATA_PORT, config_byte);
+}
+
+/*!
+ * @brief Tests the PS/2 ports
+ * @return None
+ */
+void test_ports(void)
+{
+    outb(PS2_WRITE_COMMAND_PORT, TEST_PORT_1);
+    poll_status_bit_0();
+    
+    if(inb(PS2_READ_WRITE_DATA_PORT) == PORT_TEST_PASSED)
+        printf("PS/2 Port 1 test passed\n");
+
+    outb(PS2_WRITE_COMMAND_PORT, TEST_PORT_2);
+    poll_status_bit_0();
+    
+    if(inb(PS2_READ_WRITE_DATA_PORT) == PORT_TEST_PASSED)
+        printf("PS/2 Port 2 test passed\n");   
+}
+
+/*!
+ * @brief Enables any PS/2 devices that exist and work
+ * @return None
+ */
+void enable_devices()
+{
+    outb(PS2_WRITE_COMMAND_PORT, ENABLE_PS2_PORT_1);
+    outb(PS2_WRITE_COMMAND_PORT, ENABLE_PS2_PORT_2);
+
+    printf("PS/2 devices on ports 1 and 2 enabled\n");
+
+    //Reads the config byte
+    outb(PS2_WRITE_COMMAND_PORT, READ_CONTROLLER_CONFIG_BYTE);
+
+    //Gets the config byte from the data port
+    uint8_t config_byte = inb(PS2_READ_WRITE_DATA_PORT);
+
+    //Modifies the config byte. Enables interrupts for port 1 and 2
+    config_byte |= 0x3;
+
+    //Writes the updated config byte to the data port
+    outb(PS2_WRITE_COMMAND_PORT, WRITE_CONTROLLER_CONFIG_BYTE);
+    outb(PS2_READ_WRITE_DATA_PORT, config_byte);
+
+    printf("PS/2 interrupts for ports 1 and 2 enabled\n");
+}
+
+/*!
+ * @brief Sends a byte to PS/2 port 1
+ * @param data_byte Byte to send to the port 1 device
+ * @return None
+ */
+void send_byte_port_1(uint8_t data_byte)
+{
+    poll_status_bit_1();
+    outb(PS2_READ_WRITE_DATA_PORT, data_byte);
+}
+
+/*!
+ * @brief Sends a byte to PS/2 port 2
+ * @param data_byte Byte to send to the port 2 device
+ * @return None
+ */
+void send_byte_port_2(uint8_t data_byte)
+{
+    //Set the port to be written to to port 2
+    outb(PS2_WRITE_COMMAND_PORT, WRITE_PORT_2);
+
+    poll_status_bit_1();
+    outb(PS2_READ_WRITE_DATA_PORT, data_byte);
+}
+
+/*!
+ * @brief Detects the device connected to a given PS/2 port
+ * @param port Port to detect a device on
+ * @return None
+ */
+static void detect_device(uint8_t port)
+{
+    //This uses polling... works for now, but need to properly set up interrupts
+
+    if(port == 1)
+        send_byte_port_1(0xF2);
+    else if(port == 2)
+        send_byte_port_2(0xF2);
+
+    while(inb(PS2_READ_STATUS_PORT)&0x1 != 0)
+    {
+        printf("%p ", inb(PS2_READ_WRITE_DATA_PORT));
+    }
+
+    printf("\n");
+}
+
+/*!
+ * @brief Reset devices connected to PS/2 ports
+ * @return None
+ */
+void reset_devices()
+{
+    send_byte_port_1(RESET_DEVICES);
+    send_byte_port_2(RESET_DEVICES);
 }
 
 
--- drivers/ps2/ps2.h
+++ drivers/ps2/ps2.h
@@ -4,20 +4,40 @@
 #define PS2_READ_STATUS_PORT 0x64
 #define PS2_WRITE_COMMAND_PORT 0x64
 
+#define WRITE_PORT_2 0xD4
+
 #define DISABLE_PS2_PORT_1 0xAD
 #define DISABLE_PS2_PORT_2 0xA7
 
+#define ENABLE_PS2_PORT_1 0xAE
 #define ENABLE_PS2_PORT_2 0xA8
 
 #define TEST_CONTROLLER_BYTE 0xAA
+#define TEST_PORT_1 0xAB
+#define TEST_PORT_2 0xA9
+
 #define READ_CONTROLLER_CONFIG_BYTE 0x20
 #define WRITE_CONTROLLER_CONFIG_BYTE 0x60
+
+#define SELF_TEST_PASSED 0x55
+#define PORT_TEST_PASSED 0x00
+
+#define RESET_DEVICES 0xFF
 
 
 #ifndef PS2_KEYBOARD_H
 #define PS2_KEYBOARD_H
 
+/*!
+ * @brief Checks bit 0 of the PS/2 status register until set to 1
+ * @return None
+ */
+void poll_status_port(void);
 
+/*!
+ * @brief Initializes and tests the PS/2 Controller
+ * @return None
+ */
 void init_ps2_controller();
 
 /*!
@@ -43,5 +63,38 @@ void test_ps2_controller(void);
  * @return None
  */
 void test_dual_channel(void);
+
+/*!
+ * @brief Tests the PS/2 ports
+ * @return None
+ */
+void test_ports(void);
+
+/*!
+ * @brief Enables any PS/2 devices that exist and work
+ * @return None
+ */
+void enable_devices(void);
+
+/*!
+ * @brief Sends a byte to PS/2 port 1
+ * @param data_byte Byte to send to the port 2 device
+ * @return None
+ */
+void send_byte_port_1(uint8_t data_byte);
+
+/*!
+ * @brief Sends a byte to PS/2 port 2
+ * @param data_byte Byte to send to the port 2 device
+ * @return None
+ */
+void send_byte_port_2(uint8_t data_byte);
+
+/*!
+ * @brief Reset devices connected to PS/2 ports
+ * @return None
+ */
+void reset_devices(void);
+
 
 #endif //PS2_KEYBOARD_H
